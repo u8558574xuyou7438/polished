6870d8d52e0531f1fffa84ec7f95442ad0a0d08b
Brian Hough <brian.r.hough@gmail.com>
2020-10-12 18:41:59 -0400

    fix(ellipsis): proper webkit support in multiline ellipsis

--- packages/mixin-ellipsis/src/index.ts
+++ packages/mixin-ellipsis/src/index.ts
@@ -26,7 +26,7 @@ import type { Styles } from '../../types/style'
  *   'wordWrap': 'normal'
  * }
  */
-export default function ellipsis(width?: ?string | ?number, lines?: number = 1): Styles {
+export default function ellipsis(width?: ?(string | ?number), lines?: number = 1): Styles {
   const styles = {
     display: 'inline-block',
     maxWidth: width || '100%',
@@ -39,9 +39,10 @@ export default function ellipsis(width?: ?string | ?number, lines?: number = 1):
   return lines > 1
     ? {
         ...styles,
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: lines,
         display: '-webkit-box',
-        webkitLineClamp: lines,
-        webkitBoxOrient: 'vertical',
+        whiteSpace: 'normal',
       }
     : styles
 }
